935d555e2e80f838dd3bf1bbc663ef85fe6497a6
Dona25 <166927311+Dona25@users.noreply.github.com>
2026-07-30 22:15:47 +0530

    Delete Python/program7.py

--- Python/program7.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter the number: "))
-b=int(input("Enter the number: "))
-print(f"Addition: {a+b}")
-print(f"Subtraction: {a-b}")
-print(f"Multiplication: {a*b}")
-print(f"Division: {a/b}")
-print(f"Remainder: {a%b}")
